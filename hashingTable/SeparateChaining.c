b3c3482c78986b2d4bd37238ff4c51b942c8e38a
Mark Tween <971072167@qq.com>
2024-03-13 18:18:02 +0800

    hashingTable Modification

--- hashingTable/SeparateChaining.c
+++ hashingTable/SeparateChaining.c
@@ -3,18 +3,19 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SIZE 9
 
 typedef struct ListNode {   //结点定义
     int key;
-    struct ListNode * next;
-} * Node;
+    struct ListNode *next;
+} *Node;
 
-typedef struct HashTable{   //哈希表
-    struct ListNode * table;   //这个数组专门保存头结点
-} * HashTable;
+typedef struct HashTable {   //哈希表
+    struct ListNode *table;   //这个数组专门保存头结点
+} *HashTable;
 
-void init(HashTable hashTable){
+void init(HashTable hashTable) {
     hashTable->table = malloc(sizeof(struct ListNode) * SIZE);
     for (int i = 0; i < SIZE; ++i) {
         hashTable->table[i].key = -1;   //将头结点key置为-1，next指向NULL
@@ -22,31 +23,33 @@ void init(HashTable hashTable){
     }
 }
 
-int hash(int key){   //哈希函数
+int hash(int key) {   //哈希函数
     return key % SIZE;
 }
 
-Node createNode(int key){   //创建结点专用函数
+Node createNode(int key) {   //创建结点专用函数
     Node node = malloc(sizeof(struct ListNode));
     node->key = key;
     node->next = NULL;
     return node;
 }
 
-void insert(HashTable hashTable, int key){
+void insert(HashTable hashTable, int key) {
     int hashCode = hash(key);
     Node head = hashTable->table + hashCode;   //先计算哈希值，找到位置后直接往链表后面插入结点就完事了
     while (head->next) head = head->next;
     head->next = createNode(key);   //插入新的结点
 }
-_Bool find(HashTable hashTable, int key){
+
+_Bool find(HashTable hashTable, int key) {
     int hashCode = hash(key);
     Node head = hashTable->table + hashCode;
     while (head->next && head->key != key)   //直到最后或是找到为止
         head = head->next;
     return head->key == key;   //直接返回是否找到
 }
-int main(){
+
+int main() {
     struct HashTable table;
     init(&table);
 
